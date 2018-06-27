54ff1bdd6996fb9082a15cbcd381221923036e84
Christopher Jan Riños <christopherjan1999@gmail.com>
2018-06-27 08:51:33 +0800

    Revert to 25e55d

---
+++ db/migrate/20180625035622_create_events.rb
@@ -0,0 +1,14 @@
+class CreateEvents < ActiveRecord::Migration[5.1]
+  def change
+	
+    create_table :events do |t|
+      t.integer :event_id
+      t.date :date
+      t.string :type
+      t.text :remarks
+      t.integer :doc_id
+
+      t.timestamps
+    end
+  end
+end
