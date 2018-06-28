40c2965be04c63363f9d118e182f1a1bb95804c1
faith.cerena <htiaf.anerec@gmail.com>
2018-06-28 10:35:44 +0800

    updated views

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
   root "logins#login"
-  get 'forgot' => 'logins#forget'
-  get '/dashboard' => 'logins#dashboard'
   get '/create_account' => 'account#create_account'
   get '/log_user' => 'logins#login'
+  get '/log' => 'logins#log_user'
+  get 'forgot' => 'logins#forget'
+  get '/dashboard' => 'logins#dashboard'
   get '/view_documents' => 'documents#view_documents'
   get '/view_event' => 'events#view_event'
   get '/add_document' => 'documents#add_document'
   get '/add_event' => 'events#add_event'
+  get '/folders' => 'documents#folders'
+  get '/account' => 'account#account'
   post '/redirect_account' => 'logins#login'
   post '/redirect_acct' => 'account#redirect_account'
-  get '/log' => 'logins#log_user'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
