a64aab0edf2e295f84bb495c3a2c6ae4a6b6044c
faith.cerena <htiaf.anerec@gmail.com>
2018-07-02 20:24:13 +0800

    updated views

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -10,6 +10,10 @@ def dashboard
    @countUser = User.count
    @countDocument = Document.count
    @countAttachment = Attachment.count
+   @countEventToday = Event.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+   @countDocumentToday = Document.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+   @countTransactions = @countEventToday.count + @countDocumentToday.count
+    
 end
 
 def log_user
