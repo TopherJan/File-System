90ac5c367f3eb9480aa7f5124c4fb72417d9751f
faith.cerena <htiaf.anerec@gmail.com>
2018-07-15 16:26:58 +0800

    trial google oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.secrets.google_client_id, Rails.application.secrets.google_client_secret, skip_jwt: true
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], skip_jwt: true
 end
